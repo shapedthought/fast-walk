fdeef6f627fa4916f40b0db46d93ca179f1ca275
Ed Howard <exfhoward@protonmail.com>
2022-09-04 16:21:34 +0100

    Few updates

--- src/main.rs
+++ src/main.rs
@@ -53,25 +53,30 @@ fn main() -> Result<()> {
         .parallelism(Parallelism::RayonNewPool(use_threads))
     {
         let entry = &entry?;
-        // let metadata = entry.metadata()?;
-        let extention = entry
-            .file_name()
-            .to_str()
-            .unwrap()
-            .split(".")
-            .collect::<Vec<&str>>();
 
-        let size = entry.metadata()?.len();
+        let metadata = entry.metadata()?;
 
-        extensions
-            .entry(extention.last().unwrap().to_string())
-            .and_modify(|counter| *counter += 1)
-            .or_insert(1);
+        if metadata.is_file() {
+            // let metadata = entry.metadata()?;
+            let extention = entry
+                .file_name()
+                .to_str()
+                .unwrap()
+                .split(".")
+                .collect::<Vec<&str>>();
 
-        capacity
-            .entry(extention.last().unwrap().to_string())
-            .and_modify(|counter| *counter += size as i64)
-            .or_insert(size as i64);
+            let size = entry.metadata()?.len();
+
+            extensions
+                .entry(extention.last().unwrap().to_string())
+                .and_modify(|counter| *counter += 1)
+                .or_insert(1);
+
+            capacity
+                .entry(extention.last().unwrap().to_string())
+                .and_modify(|counter| *counter += size as i64)
+                .or_insert(size as i64);
+        }
     }
 
     let mut count_vec: Vec<(&String, &i32)> = extensions.iter().collect();
@@ -105,14 +110,16 @@ fn main() -> Result<()> {
         wtr.write_record(&[i.0.to_string(), i.1.to_string(), cap.1.to_string()])?;
     }
     wtr.flush()?;
-    let total_files = count_vec.iter().count();
+    let total_files = count_vec.iter().map(|x| x.1).fold(0, |a, x| a + x);
     let total_cap = sum_vec.iter().map(|x| x.1).fold(0, |a, x| a + x);
 
     // sleep(Duration::from_secs(10));
-
     sp.stop();
-    let duration = start.elapsed();
-    println!("\nThat took: {:?}", duration);
+
+    let files_hour = (total_files as f32 / start.elapsed().as_secs_f32()) * 3600.00;
+
+    println!("\nThat took: {:?}", start.elapsed());
+    println!("Estimated files per-hour: {} {}", files_hour, emoji::travel_and_places::sky_and_weather::FIRE.glyph);
 
     println!(
         "\nTotal Files: {}, Total Cap: {} MB",
